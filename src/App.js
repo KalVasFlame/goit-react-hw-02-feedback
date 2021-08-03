f3fd0810554a9210751e200b37d37e7d4aa1805b
KalVasFlame <unnameddwar@gmail.com>
2021-08-03 12:21:52 +0300

    перевел на хуки)

--- src/App.js
+++ src/App.js
@@ -1,54 +1,45 @@
-import React, { Component } from 'react';
-import Section from './components/Section';
-import Statistics from './components/Statistics';
+import { useState } from "react"
 
+import Section from "./components/Section"
+import Statistics from "./components/Statistics"
+import FeedbackOptions from "./components/FeedbackOptions"
 
+import "./App.css"
 
-import './App.css';
-import FeedbackOptions from './components/FeedbackOptions';
+export default function App() {
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
 
+  const positivePercentage = Math.round((good / (good + neutral + bad)) * 100)
+  const total = good + neutral + bad
 
+  const onBtnCLick = (name) => () => {
+    switch (name) {
+      case "good":
+        return setGood((prev) => prev + 1)
+      case "bad":
+        return setBad((prev) => prev + 1)
+      case "neutral":
+        return setNeutral((prev) => prev + 1)
 
-
-class App extends Component {
-
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-onBtnCLick = (name) => () => {
-    this.setState(prev => ({
-      [name]: prev[name] + 1,
-    }))
-  };
-  
-
-
-  render() {
-const { good, neutral, bad } = this.state;
-    const positivePercentage = Math.round(good / (good + neutral + bad) * 100);
-    const total = good + neutral + bad;
-    return (
-      <>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions onGood={this.onBtnCLick('good')}
-            onNeutral={this.onBtnCLick('neutral')}
-            onBad={this.onBtnCLick('bad')} />
-        </ Section >
-        <Section title={'Statistics'}>
-          {total>0? <Statistics good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          /> : <h3>not given feedback</h3>}
-        </ Section  >
-      </>
-    );
+      default:
+        break
+    }
   }
-};
 
-
-export default App;
+  return (
+    <>
+      <Section title={"Please leave feedback"}>
+        <FeedbackOptions onGood={onBtnCLick("good")} onNeutral={onBtnCLick("neutral")} onBad={onBtnCLick("bad")} />
+      </Section>
+      <Section title={"Statistics"}>
+        {total > 0 ? (
+          <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />
+        ) : (
+          <h3>not given feedback</h3>
+        )}
+      </Section>
+    </>
+  )
+}
